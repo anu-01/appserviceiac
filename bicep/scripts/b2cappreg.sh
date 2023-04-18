7dcf71992b28ced85bf950ca5d354f7458163a1a
RussSmi <russell_smith@hotmail.com>
2023-04-18 15:37:48 +0100

    Add uris

--- bicep/scripts/b2cappreg.sh
+++ bicep/scripts/b2cappreg.sh
@@ -4,8 +4,12 @@
 az login --service-principal -u $3 -p $4 --allow-no-subscriptions --tenant $1
 
 AppName="$2appreg"    
+MainUri="https://rusmithb2c.b2clogin.com/rusmithb2c.onmicrosoft.com/oauth2/authresp"
+SignUpInUri="https://$2.azurewebsites.net/B2C_1_signupsignin1"
+SignInOidcUri="https://$2.azurewebsites.net/signin-oidc"
 
-az ad app create --display-name $AppName
+
+az ad app create --display-name $AppName --web-redirect-uris MainUri SignUpInUri SignInOidcUri
     
 az ad app list --display-name $AppName #(Gives details of newly created app)
     
